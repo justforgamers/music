bdb8d82d385b141f4a6407f5b929d08e3fb33db3
justforgamers <45237111+justforgamers@users.noreply.github.com>
2019-03-26 19:45:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('ready', () => {
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`JustForGamers`)
-client.user.setStatus("dnd")
+client.user.setStatus("https://www.twitch.tv/justforgamerstv")
 });
 
 
